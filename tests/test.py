284dc488b15aaf2fda194ebf89cf7861038d67a1
SwirtaB <git@swirta.pl>
2021-05-31 13:23:24 +0200

    Added testing scrpit

--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,81 @@
+#!/usr/bin/python3
+
 import os
 import sys
+from typing import List
+from multiprocessing import Process
 
-def run_tests():
+#mnijeszy krok dla algorytmu brutalnego
+bruteStep = 1
+#mniejsza liczba iteracji dla algorytmu brutalnego
+bruteIterations = 3
+
+#krok
+step = 10
+#liczba iteracji
+iterations = 100
+#poczatkowy rozmiar problemu
+initProblemSize = 10
+#typy generatorów 
+generators = ["-gp", "-gs", "-gns"]
+#folder 
+folderName = "test_results"
+
+
+def test_universal_sort(executable : str, generator : str):
+    filename = "universal_sort_" + "generator:%s_step:%d_iterations:%d.txt"%(generator, step, iterations)
+    filePath = folderName + '/' + filename
+    params = "-t %s %d %d %d -us -f %s"%(generator, initProblemSize ,step, iterations, filePath)
+    exe = executable + ' ' + params
+    
+    os.system(exe)
+
+def test_substring_sort(executable : str, generator : str):
+    filename = "substring_sort_" + "generator:%s_step:%d_iterations:%d.txt"%(generator, step, iterations)
+    filePath = folderName + '/' + filename
+    params = "-t %s %d %d %d -ss -f %s"%(generator, initProblemSize ,step, iterations, filePath)
+    exe = executable + ' ' + params
+    
+    os.system(exe)
+
+def test_brute_sort(executable : str, generator : str):
+    filename = "brute_force_sort_" + "generator:%s_step:%d_iterations:%d.txt"%(generator, bruteStep, bruteIterations)
+    filePath = folderName + '/' + filename
+    params = "-t %s %d %d %d -bs -f %s"%(generator, initProblemSize ,bruteStep, bruteIterations, filePath)
+    exe = executable + ' ' + params
+    
+    os.system(exe)
+
+def main():
     try:
-        os.mkdir("test_results")
+        os.mkdir(folderName)
     except FileExistsError:
         pass
 
     if sys.platform.startswith("win"):
-        executable = "powershell.exe ./bin/aal_cmyk"
+        executable = "powershell.exe ./bin/aal_cmyk "
     else:
         executable = "./bin/aal_cmyk"
-    
-    universalFile = " -f test_results/universal_sort_parametric.txt"
-    substringFile = " -f test_results/substring_sort_parametric.txt"
-    bruteFile = " -f test_results/brute_sort_parametric.txt"
 
-    os.system(executable + " -t -gp 10 3 2 -us" + universalFile)
-    os.system(executable + " -t -gp 10 3 2 -ss" + substringFile)
-    os.system(executable + " -t -gp 10 3 2 -bs" + bruteFile)
+    processes : List[Process] = []
+
+    for generator in generators:
+        p = Process(target=test_universal_sort, args=(executable, generator))
+        processes.append(p)
+
+    for generator in generators:
+        p = Process(target=test_substring_sort, args=(executable, generator))
+        processes.append(p)
+
+    for generator in generators:
+        p = Process(target=test_brute_sort, args=(executable, generator))
+        processes.append(p)
+
+    for process in processes:
+        process.start()
+    
+    for process in processes:
+        process.join()
 
 if __name__ == "__main__":
-    run_tests()
+    main()
